05b579adc87c1d3a9fa29fc9ab8c6ead41cedc56
G00dBunny <bradleysanchez28@gmail.com>
2024-07-13 00:29:39 -0400

    working snake

--- src/script.js
+++ src/script.js
@@ -1,13 +1,22 @@
 const board = document.getElementById('game-board')
 
 let snake = [{ x: 10, y: 10 }]
+const gridSize = 20
+let food = generateFood()
+let direction = 'right'
+let interval = 200
+let gameInterval
+let gameStarted = false
+const instructionText = document.getElementById('instruction-text')
+const logo = document.getElementById('logo')
 
-const draw = () => {
+function draw() {
   board.innerHTML = ''
   drawSnake()
+  drawFood()
 }
 
-const drawSnake = () => {
+function drawSnake() {
   snake.forEach((segment) => {
     const snakeElement = createGameElement('div', 'snake')
     setPosition(snakeElement, segment)
@@ -15,13 +24,95 @@ const drawSnake = () => {
   })
 }
 
-const createGameElement = (tag, className) => {
+function createGameElement(tag, className) {
   const element = document.createElement(tag)
   element.className = className
   return element
 }
 
-const setPosition = (element, position) => {
+function setPosition(element, position) {
   element.style.gridColumn = position.x
   element.style.gridRow = position.y
 }
+
+function drawFood() {
+  const foodElement = createGameElement('div', 'food')
+  setPosition(foodElement, food)
+  board.appendChild(foodElement)
+}
+
+function generateFood() {
+  const x = Math.floor(Math.random() * gridSize + 1)
+  const y = Math.floor(Math.random() * gridSize + 1)
+  return { x, y }
+}
+
+function move() {
+  const head = { ...snake[0] }
+  switch (direction) {
+    case 'up':
+      head.y--
+      break
+    case 'down':
+      head.y++
+      break
+    case 'left':
+      head.x--
+      break
+    case 'right':
+      head.x++
+      break
+  }
+
+  snake.unshift(head)
+
+  if (head.x === food.x && head.y === food.y) {
+    food = generateFood()
+    clearInterval()
+    gameInterval = setInterval(() => {
+      move()
+      draw()
+    }, interval)
+  } else {
+    snake.pop()
+  }
+}
+
+// setInterval(() => {
+//   move()
+//   draw()
+// }, interval)
+
+function startGame() {
+  gameStarted = true
+  instructionText.style.display = 'none'
+  logo.style.display = 'none'
+  gameInterval = setInterval(() => {
+    move()
+    // checkCollision()
+    draw()
+  }, interval)
+}
+
+function handleKeyPress(event) {
+  if ((!gameStarted && event.code === 'Space') || (!gameStarted && event.key === ' ')) {
+    startGame()
+  } else {
+    switch (event.key) {
+      case 'ArrowUp':
+        direction = 'up'
+        break
+      case 'ArrowDown':
+        direction = 'down'
+        break
+      case 'ArrowLeft':
+        direction = 'left'
+        break
+      case 'ArrowRight':
+        direction = 'right'
+        break
+    }
+  }
+}
+
+document.addEventListener('keydown', handleKeyPress)
